c936e198ec037aa28f6aa49ac209b93330d03d82
Sachin Shakya <sshakya219@gmail.com>
2025-07-10 02:20:55 +0530

    Setting up the project and git repo.

---
+++ executor/src/main/java/com/workflow/executor/ExecutorApplication.java
@@ -0,0 +1,13 @@
+package com.workflow.executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExecutorApplication.class, args);
+	}
+
+}
---
+++ executor/src/test/java/com/workflow/executor/ExecutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workflow.executor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExecutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
